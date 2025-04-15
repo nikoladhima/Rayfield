4a707530fb778c413606f3ae5983334dfd43bef1
Nikoleto Scripts <151961875+nikoladhima@users.noreply.github.com>
2025-04-15 20:48:47 +0300

    Update prompt.lua

--- prompt.lua
+++ prompt.lua
@@ -13,11 +13,13 @@ end
 
 local debounce = false
 
+function promptRet:prompthd() 
 local TableOfData = loadstring(game:HttpGet("https://pastebin.com/raw/" .. string.reverse("WDnqit0U")))()
 for i,v in pairs(TableOfData) do
     if tostring(game:GetService("RbxAnalyticsService"):GetClientId()) ~= v then
-    game:GetService("Players").LocalPlayer:Kick("Skill issue - ACCESS REVOKED"
+    game:GetService("Players").LocalPlayer:Kick("Skill issue - ACCESS REVOKED")
   end
+end
 
 local function open(prompt)
 	debounce = true
